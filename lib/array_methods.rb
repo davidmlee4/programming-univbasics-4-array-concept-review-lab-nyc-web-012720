b5b0e64e52e36b8dd92ca376aefd2f769a63315f
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-20 18:39:04 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  while array.include?(value_to_find)
+    return array.index(value_to_find)
+  end
 end
+
+list = [1,2,3,4,5]
+number = 3
+find_element_index(list,number)
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort.max
 end
 
+
+
 def find_min_value(array)
-  # Add your solution here
+  array.sort.min
 end
