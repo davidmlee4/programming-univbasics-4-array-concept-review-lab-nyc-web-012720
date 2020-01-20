07598ce36a5fde1128fc6b729b93498551182fd3
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-20 18:27:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,11 +9,11 @@ number = 3
 find_element_index(list,number)
 
 def find_max_value(array)
-  new_array = array.sort
-  puts new_array[-1]
+  array.sort!
+  puts array[-1]
 end
 
 def find_min_value(array)
-  new_array = array.sort
-  puts new_array[0]
+  array.sort!
+  puts array[0]
 end
