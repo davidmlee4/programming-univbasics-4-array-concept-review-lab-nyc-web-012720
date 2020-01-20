6248f63d5d3dd407ce259221bf68fb6a3169184d
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-20 18:23:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
-  while array.include?(value_to_find) && array.index(value_to_find) ==
-    return
+  while array.include?(value_to_find) 
+    return array.index(value_to_find)
   end
 end
 
