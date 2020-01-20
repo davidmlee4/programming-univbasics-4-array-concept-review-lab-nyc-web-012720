bfbf476b5c327c4dc39391287926ef62812686be
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-20 18:22:34 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,6 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  if array[counter] == value_to_find
-    puts array[counter]
-    counter += 1
+  while array.include?(value_to_find) && array.index(value_to_find) ==
+    return
   end
 end
 
